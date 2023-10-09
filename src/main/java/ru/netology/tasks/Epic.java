58eb305cb1cca02fc2873045b11da35f36929308
Mikhail <mikle0240@gmail.com>
2023-10-09 18:20:33 +0300

    Add Epic.java

---
+++ src/main/java/ru/netology/tasks/Epic.java
@@ -0,0 +1,14 @@
+package ru.netology.tasks;
+
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+}

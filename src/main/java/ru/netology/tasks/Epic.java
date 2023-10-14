0e571f3146b8a5b142906b7c9cd345c7593032e4
Mikhail <mikle0240@gmail.com>
2023-10-14 09:32:46 +0300

    Modified Epic.java

--- src/main/java/ru/netology/tasks/Epic.java
+++ src/main/java/ru/netology/tasks/Epic.java
@@ -2,6 +2,7 @@ package ru.netology.tasks;
 
 public class Epic extends Task {
     protected String[] subtasks;
+
     public Epic(int id, String[] subtasks) {
         super(id);
         this.subtasks = subtasks;
